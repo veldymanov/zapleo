3d925877dd351d42ef3bca88f9ae1259fa5ca839
Veldymanov <veldymanov@gmail.com>
2017-10-06 16:56:28 +0300

    fix testimonial.html item render bug

--- src/js/typed-custom.js
+++ src/js/typed-custom.js
@@ -1,12 +1,10 @@
 $(document).ready(function () {	
-	$('.content figure').on ('mouseover', function () {
-		var self = this;
-		//Typed.new('.content figure .review>p>span', {
-		$(self).find('.review>p>span').typed({
-			stringsElement: $(self).find('.typed-strings'),
-			startDelay: 300,
-			backSpeed: 0,
-			backDelay: 500
-		});
-	});
+    $(".content figure").on ('mouseover', function () {
+        Typed.new(".content figure .review>p>span", {
+            stringsElement: $("#typed-strings"),
+            startDelay: 300,
+            backSpeed: 0,
+            backDelay: 500
+        })
+    });
 });
