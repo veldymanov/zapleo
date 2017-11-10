005f6e4a3ad779a4990033d9c0634c6777337bd3
Veldymanov <veldymanov@gmail.com>
2017-11-10 16:51:21 +0200

    team.html

--- src/js/team.js
+++ src/js/team.js
@@ -1,21 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let resumes = document.querySelectorAll('.js-resume-open-pop-up');
-    for(let resume of resumes){
-        resume.addEventListener('click', function(){
+    let resumeBtns = document.querySelectorAll('.js-resume-open-pop-up');
+    for(let resumeBtn of resumeBtns){
+        resumeBtn.addEventListener('click', function(){
             this.closest('li').querySelector('.js-wrap-popup').style.display = "block";
             this.closest('li').querySelector('.js-popup-resume').style.display = "block";
             //stop scroll propagation
-            document.querySelector("body").style.overflow = 'hidden';
+            document.querySelector("body").style.cssText = "overflow-y: hidden; position: fixed";
         })
     }
 
     let popups = document.querySelectorAll('.wrap-popup');
     for(let popup of popups){
-        popup.addEventListener('click', function(){
+        popup.addEventListener('click', function(e){
+            e.preventDefault();
+
             this.closest('li').querySelector('.js-popup-resume').style.display = "none";
             this.closest('li').querySelector('.js-wrap-popup').style.display = "none";           
             
-            document.querySelector("body").style.overflow = 'auto';
-        })        
+            document.querySelector("body").style.cssText = "overflow-y: auto; position: inherit";
+        })
     }
 })
