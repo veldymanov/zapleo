b677af54c1b664f064c3f2bbb0bbb6457afaa5c6
Veldymanov <veldymanov@gmail.com>
2018-01-16 09:42:03 +0200

    service.html jquery removed

--- src/js/services.js
+++ src/js/services.js
@@ -1,45 +1,21 @@
-// Find elements shown on screen
-function inWindow(s){
-	let scrollTop = window.pageYOffset;
-	let windowHeight1 = window.innerHeight;
-	let currentEls1 = document.querySelectorAll(s);
-	let result1 = [];
-	/*
-	var	windowHeight = $(window).height(),
-		currentEls = $(s),
-		result = [];	
-	*/
-	currentEls1.forEach( el => {
-		let elTop1 = el.offsetTop;
-		let elTop2 = el.getBoundingClientRect();
-		let elHeight1 = el.offsetHeight;
-		if(scrollTop <= elTop1 && (elHeight1 + elTop1) < (scrollTop + windowHeight1)) {
-			result1.push(el);
-	//		console.log(el);
-		}				
-	});
-	//	console.log(currentEls1);	
-	console.log('js: ', result1);
-	/*	
-	currentEls.each(function(){
-		var el = $(this);
-		var offset = el.offset();
-		var elHeight = el.height();
-		if(scrollTop <= offset.top && (elHeight + offset.top) < (scrollTop + windowHeight)) {
-			result.push(this);
-		}
-	});
-	//	console.log(currentEls);	
-	console.log('jQ: ', result);
-	*/
-  	return result1;
-}
-
-let tiles = inWindow('.js-tile');
-tiles.forEach( tile => tile.classList.add("showtext"));
-
 // Show text on scroll
 window.addEventListener('scroll', function(){
 	let tiles = inWindow('.js-tile');
 	tiles.forEach( tile => tile.classList.add("showtext"));
 });
+
+function inWindow(s){
+	let windowHeight = window.innerHeight;
+	let currentEls = document.querySelectorAll(s);
+	let result = [];
+
+	currentEls.forEach( el => {
+		let elTop = el.getBoundingClientRect();
+
+		if( ( windowHeight - elTop.top > 0.8 * elTop.height ) && !el.classList.contains("showtext") ) {
+			result.push(el);
+		}	
+	});
+
+  return result;
+}
