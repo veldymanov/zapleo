04dc2c19c9ce8e08f404712bc5db84804a5a5fc2
Veldymanov <veldymanov@gmail.com>
2018-01-24 13:15:06 +0200

    new feature: team.html loading spinner

--- src/js/index.js
+++ src/js/index.js
@@ -40,6 +40,6 @@ $(window).load(function () {
 //////////////////////////////////////////////////////////////
 if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('service-worker.js')
-        .then( reg => console.log('Service Worker registration succeeded. Scope is ' + reg.scope) )
-		.catch( error => console.log('Registration failed' + error) )
+      .then( reg => console.log('Service Worker registration succeeded. Scope is ' + reg.scope) )
+		  .catch( error => console.log('Registration failed' + error) )
 }
