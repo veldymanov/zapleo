49e6bdea2ce92a61814ea6beace0805d01c089a6
Veldymanov <veldymanov@gmail.com>
2018-01-25 12:43:55 +0200

    bug fix, team.js

--- src/js/team.js
+++ src/js/team.js
@@ -1,23 +1,21 @@
 document.addEventListener("DOMContentLoaded", function() {
-    let resumeBtns = document.querySelectorAll('.js-resume-open-pop-up');
-    for(let resumeBtn of resumeBtns){
-        resumeBtn.addEventListener('click', function(){
-            this.closest('li').querySelector('.js-wrap-popup').style.display = "block";
-            this.closest('li').querySelector('.js-popup-resume').style.display = "block";
-            //stop scroll propagation
-            document.querySelector("body").style.cssText = "overflow-y: hidden; position: fixed;";
-        })
-    }
+  const resumeBtns = document.querySelectorAll('.js-resume-open-pop-up');
+  for(let resumeBtn of resumeBtns){
+    resumeBtn.addEventListener('click', function(){
+      this.closest('li').querySelector('.js-wrap-popup').style.display = "block";
+      this.closest('li').querySelector('.js-popup-resume').style.display = "block";
+      //stop scroll propagation
+      document.querySelector("body").style.cssText = "overflow-y: hidden;";
+    })
+  }
 
-    let popups = document.querySelectorAll('.wrap-popup');
-    for(let popup of popups){
-        popup.addEventListener('click', function(e){
-            e.preventDefault();
-
-            this.closest('li').querySelector('.js-popup-resume').style.display = "none";
-            this.closest('li').querySelector('.js-wrap-popup').style.display = "none";           
-            
-            document.querySelector("body").style.cssText = "overflow-y: auto; position: inherit;";
-        })
-    }
+  const popups = document.querySelectorAll('.wrap-popup');
+  for(let popup of popups){
+    popup.addEventListener('click', function(e){
+        e.preventDefault();
+        this.closest('li').querySelector('.js-popup-resume').style.display = "none";
+        this.closest('li').querySelector('.js-wrap-popup').style.display = "none";
+        document.querySelector("body").style.cssText = "overflow-y: auto;";
+    })
+  }
 })
