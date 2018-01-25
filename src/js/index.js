ffc26f621c3e95a662877e534330c6607b627f57
Veldymanov <veldymanov@gmail.com>
2018-01-25 10:47:51 +0200

    small standardization

--- src/js/index.js
+++ src/js/index.js
@@ -1,37 +1,34 @@
 $(window).load(function () {
-	///////////////////////////
-	//Parallax
-	///////////////////////////
+	/**
+	* Parallax
+	*/
+  //Parallax init
+  $('#menorah').parallax({
+  //    calibrateX: true,
+  //    calibrateY: true,
+      limitX: 38,
+      limitY: 45
+  });
+  //Calculating parallax height
+  function calcParallaxHeight() {
+      //Cut 'bugs' on mobile
+      $('#menorah').height($('#menorah img:first').height() - 3);
+      //set section height
+      $('.start').height( $('#menorah img:first').height() - 3);
+  }
+  calcParallaxHeight();
+  //Updating parallax height onresize
+  $(window).resize(calcParallaxHeight);
 
-    //Parallax images visible after all imgs are loaded
-    setTimeout( function(){
-    	$('#menorah').css('visibility','visible');
-    }, 0);
+  //Scroll to <main>
+  $('.mouse').on('click', function(){
+      $('html, body').animate({scrollTop: $('main').offset().top + 15}, 600);
+  });
 
-    //////////////////
-    //Parallax init
-    //////////////////
-    $('#menorah').parallax({
-    //    calibrateX: true,
-    //    calibrateY: true,
-        limitX: 38,
-        limitY: 45
-    });
-    //Calculating parallax height
-    function calcParallaxHeight() {
-        //Cut 'bugs' on mobile
-        $('#menorah').height($('#menorah img:first').height() - 3);
-        //set section height
-        $('.start').height( $('#menorah img:first').height() - 3);
-    }
-    calcParallaxHeight();
-    //Updating parallax height onresize
-    $(window).resize(calcParallaxHeight);
-
-    //Scroll to <main>
-    $('.mouse').on('click', function(){
-        $('html, body').animate({scrollTop: $('main').offset().top + 15}, 600);
-    });
+  //Parallax images visible after all imgs are loaded
+  setTimeout( function(){
+    $('#menorah').css('visibility','visible');
+  })
 });
 
 
