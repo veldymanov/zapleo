c9bd01d9aaae08c0180d8f6932ecbc9566fc57d6
Veldymanov <veldymanov@gmail.com>
2018-02-27 10:22:00 +0200

    build for github

--- gulpfile.js
+++ gulpfile.js
@@ -113,7 +113,7 @@ gulp.task('build:copy', ['build:cleanfolder'], () =>
 gulp.task('url:adjust', ['build:copy'], () =>
   gulp.src('docs/css/*.css')
   .pipe(urlAdjuster({
-    replace:  ['../','/'], // for github pages: "replace:  ['../','']"
+    replace:  ['../',''], // for github pages: "replace:  ['../','']"
   }))
   .pipe(gulp.dest('docs/css/'))
 );
